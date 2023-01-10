876c881c6166f0d7e21efcdec4e5e50959ad17aa
raulgsf <116838893+raulgsf@users.noreply.github.com>
2023-01-10 17:16:05 -0300

    Add files via upload

---
+++ src/dominio/Monitoramento.java
@@ -0,0 +1,56 @@
+package dominio;
+
+import java.math.BigDecimal;
+
+public class Monitoramento {
+	String dados;
+	String skwurl;
+	String skwuser;
+	String skwpassword;
+	BigDecimal codparc;
+	
+	public String getDados() {
+		return dados;
+	}
+
+	public void setDados(String dados) {
+		this.dados = dados;
+	}
+	
+	public String getSkwurl() {
+		return skwurl;
+	}
+	
+	public void setUrl(String skwurl) {
+		this.skwurl = skwurl;
+	}
+	
+	public String getSkwuser() {
+		return skwuser;
+	}
+
+	public void setSkwuser(String skwuser) {
+		this.skwuser = skwuser;
+	}
+
+	public String getSkwpassword() {
+		return skwpassword;
+	}
+
+	public void setSkwpassword(String skwpassword) {
+		this.skwpassword = skwpassword;
+	}
+
+	public BigDecimal getCodparc() {
+		return codparc;
+	}
+
+	public void setCodparc(BigDecimal bigDecimal) {
+		this.codparc = bigDecimal;
+	}
+
+	@Override
+	public String toString() {
+		return "Dados [dados=" + skwuser + skwpassword + codparc + "]";
+	}
+}
---
+++ src/io/code/myapi/ServicoAPI.java
@@ -0,0 +1,139 @@
+package io.code.myapi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import br.com.sankhya.jape.core.JapeSession;
+import br.com.sankhya.jape.core.JapeSession.SessionHandle;
+import dominio.Historico;
+
+public class ServicoAPI {
+	
+	public static HttpURLConnection montarConexao(String skw_user, String skw_password, String skw_url, BigDecimal skw_company_id) throws IOException {
+		URL url = new URL(skw_url);
+
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+
+        JsonObject requestBody = new JsonObject();
+        requestBody.addProperty("SKW_USER", skw_user);
+        requestBody.addProperty("SkW_PASSWORD", skw_password);
+        requestBody.addProperty("SkW_COMPANY_ID", skw_company_id);
+
+        OutputStream os = conn.getOutputStream();
+        byte[] input = requestBody.toString().getBytes("utf-8");
+        os.write(input, 0, input.length);
+        os.flush();
+        os.close();
+
+        conn.connect();
+        
+        return conn;
+    }
+	
+	public static void monitorar(String skw_user, String skw_password, String skw_url, BigDecimal codparc) throws Exception {
+		
+		try {
+			HttpURLConnection conexao = montarConexao(skw_user, skw_password, skw_url, codparc);
+			
+			BufferedReader resposta = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+			String jsonEmString = Util.converteJsonEmString(resposta);
+			
+			Gson gson = new Gson();
+			Historico historico = gson.fromJson(jsonEmString, Historico.class);
+			
+			imprimir(historico);
+			
+		} catch (Exception e) {
+			throw new Exception("ERRO: " + e);
+		}
+		
+	}
+	
+	public static void salvarInformacoes() {
+		SessionHandle hnd = null;
+		
+		try {
+			hnd = JapeSession.open();
+			
+			
+		} finally {
+		    JapeSession.close(hnd);
+		}
+		
+	}
+	
+	
+	
+	private static void imprimir(Historico historico) {
+		System.out.println("codParc: " + historico.getCodparc());
+		System.out.println("negocio: " + historico.getNegocio());
+		System.out.println("versaoSistema: " + historico.getVersaosistema());
+		System.out.println("banco: " + historico.getBanco());
+		System.out.println("descrição: " + historico.getExtensao().getDescricao());
+		System.out.println("versão descrição: " + historico.getExtensao().getVersao());
+		System.out.println("data hora instalacao: " + historico.getExtensao().getDataHoraInstalacao());
+		System.out.println("data hora build: " + historico.getExtensao().getDataHoraBuild());
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
